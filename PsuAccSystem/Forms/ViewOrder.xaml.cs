7e8f4992849a52ebf949d56ac7bac37d27cc75aa
MatushkinaAnastasia <46565861+MatushkinaAnastasia@users.noreply.github.com>
2021-05-20 12:56:28 +0500

    init

---
+++ PsuAccSystem/Forms/ViewOrder.xaml.cs
@@ -0,0 +1,43 @@
+﻿using PsuAccSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PsuAccSystem.Forms
+{
+	/// <summary>
+	/// Логика взаимодействия для ViewOrder.xaml
+	/// </summary>
+	public partial class ViewOrder : Window
+	{
+		public ViewOrder()
+		{
+			InitializeComponent();
+			Orders = new ObservableCollection<Order>
+			{
+				new Order(1, "привет", "вася", "раевский", "15/15/15", "15:34"),
+				new Order(2, "приседания", "миша", "раевский", "15/15/15", "15:35"),
+				new Order(4, "привет", "вася", "раевский", "11/15/15", "11:34"),
+				new Order(1, "привет", "вася", "раевский", "15/15/15", "15:34"),
+				new Order(2, "приседания", "миша", "раевский", "15/15/15", "15:35"),
+				new Order(4, "привет", "вася", "раевский", "11/15/15", "11:34"),
+				new Order(1, "привет", "вася", "раевский", "15/15/15", "15:34"),
+				new Order(2, "приседания", "миша", "раевский", "15/15/15", "15:35"),
+				new Order(4, "привет", "вася", "раевский", "11/15/15", "11:34"),
+			};
+
+			DataContext = this;
+		}
+
+		public ObservableCollection<Order> Orders { get; private set; }
+	}
+}
---
+++ PsuAccSystem/Model/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PsuAccSystem.Model
+{
+	public class Order
+	{
+		public Order(long id, string service, string customer, string worker, string date, string time)
+		{
+			Id = id;
+			Service = service;
+			Customer = customer;
+			Worker = worker;
+			Date = date;
+			Time = time;
+		}
+
+		public long Id { get; set; }
+		public string Service { get; set; }
+		public string Customer { get; set; }
+		public string Worker { get; set; }
+		public string Date { get; set; }
+		public string Time { get; set; }
+	}
+}
